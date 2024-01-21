43dcaf2fab7f39499ef46a4be8fbec940988bee5
MicheleFiorentino <fiorentino.mic@outlook.it>
2024-01-21 17:07:46 +0100

    Created method for the schematic and template file

--- projects/mfs-lib/schematics/custom-lib/index.ts
+++ projects/mfs-lib/schematics/custom-lib/index.ts
@@ -1,7 +1,22 @@
-import { Rule, Tree, SchematicContext } from '@angular-devkit/schematics';
+import { apply, applyTemplates, chain, mergeWith, Rule, Tree, SchematicContext, url, move } from '@angular-devkit/schematics';
+import { strings, normalize } from '@angular-devkit/core';
+import { CustomLibSchema } from './custom-lib';
 
-export function customLiBGenerator(): Rule {
+export function customLiBGenerator(options: CustomLibSchema): Rule {
   return (tree: Tree, context: SchematicContext) => {
+    const templateSource = apply(
+      url('./files'), [
+        applyTemplates({
+          classify: strings.classify,
+          dasherize: strings.dasherize,
+          name: options.name
+        }),
+        move(normalize(`/${options.path}/${strings.dasherize(options.name)}`))
+      ]
+    );
 
+    return chain([
+      mergeWith(templateSource)
+    ]);
   }
 }
