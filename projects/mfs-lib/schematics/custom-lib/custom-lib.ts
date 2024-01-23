14ea3d75e1031d2ad161df3fd93083bec3990250
MicheleFiorentino <fiorentino.mic@outlook.it>
2024-01-23 22:06:45 +0100

    Library with template

--- projects/mfs-lib/schematics/custom-lib/custom-lib.ts
+++ projects/mfs-lib/schematics/custom-lib/custom-lib.ts
@@ -1,4 +1,5 @@
 export interface CustomLibSchema {
   name: string;
-  path: string;
+  project: string;
+  style: string;
 }
--- projects/mfs-lib/schematics/custom-lib/index.ts
+++ projects/mfs-lib/schematics/custom-lib/index.ts
@@ -1,9 +1,13 @@
-import { apply, applyTemplates, chain, mergeWith, Rule, url, move } from '@angular-devkit/schematics';
+import { Rule, Tree, SchematicContext, externalSchematic, chain, mergeWith, MergeStrategy, move, applyTemplates, apply, url } from '@angular-devkit/schematics';
 import { strings, normalize } from '@angular-devkit/core';
 import { CustomLibSchema } from './custom-lib';
 
 export function customLibGenerator(options: CustomLibSchema): Rule {
-  return () => {
+  return (tree: Tree, context: SchematicContext) => {
+
+    context.logger.info('Generating custom library...');
+    tree;
+
     const templateSource = apply(
       url('./files'), [
         applyTemplates({
@@ -11,12 +15,17 @@ export function customLibGenerator(options: CustomLibSchema): Rule {
           dasherize: strings.dasherize,
           name: options.name
         }),
-        move(normalize(`/${options.path}/${strings.dasherize(options.name)}`))
+        move(normalize(`${options.project}/${strings.dasherize(options.name)}/src/lib/`))
       ]
     );
 
     return chain([
-      mergeWith(templateSource)
+      externalSchematic('@schematics/angular', 'library', {
+        name: options.name,
+        projectRoot: normalize(`${options.project}/${strings.dasherize(options.name)}`)
+      }),
+      mergeWith(templateSource, MergeStrategy.Overwrite)
     ]);
+
   }
 }
